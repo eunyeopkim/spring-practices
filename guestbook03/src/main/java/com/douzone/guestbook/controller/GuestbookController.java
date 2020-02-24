4413ac486db68832ca47583f56ab2f418336bbf3
bit-user <bit-user@DESKTOP-2837PPH>
2020-02-24 09:49:41 +0900

    guestbook03 module 추가

---
+++ guestbook03/src/main/java/com/douzone/guestbook/controller/GuestbookController.java
@@ -0,0 +1,16 @@
+package com.douzone.guestbook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GuestbookController {
+	
+	@ResponseBody
+	@RequestMapping("")
+	public String index() {
+		return "guestbook list";
+	}
+	
+}
